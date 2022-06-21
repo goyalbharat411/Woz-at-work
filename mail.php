9051866c3eec9610a06d80d958230fa50eb54439
unknown <goyalbharat411@gmail.com>
2022-06-21 11:16:22 +0530

    21commit

--- mail.php
+++ mail.php
@@ -1,24 +1,28 @@
 <?php
+ require("PHPMailer/src/PHPMailer.php");
+ require("PHPMailer/src/SMTP.php");
 
-$mail = new PHPMailer;
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail = new PHPMailer\PHPMailer\PHPMailer(true);
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
+
+$mail->SMTPDebug = 4; //                              // debugging: 1 = errors and messages, 2 = messages only
+$mail->Mailer = "smtp";
+ $mail->isHTML(true); // enable SMTP                     // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'goyalbharat411@gmail.com';                   // SMTP username
 $mail->Password = 'goyal@505';                             // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->Port = 465;                                    // TCP port to connect to
 
-$mail->setFrom('goyalbharat411@gmail.com', 'Mailer');
-$mail->addAddress('bharatg@rkcl.in', 'Joe User');     // Add a recipient
+$mail->setFrom('goyalbharat411@gmail.com', 'Woz At Work');
+$mail->addAddress('bharat@rkcl.in');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 
-$mail->addAttachment('/home/tutsplanet/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/home/tutsplanet/file.tar.gz');         // Add attachments
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Here comes the the subject';
